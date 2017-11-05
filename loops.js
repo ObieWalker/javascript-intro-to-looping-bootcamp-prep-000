5d731d10730a9d9345764fc512c175d0c1ee6dfc
Obi <obinnawalker@gmail.com>
2017-11-05 18:18:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,3 +16,13 @@ function forLoop(array) {
     }
     return "done";
   }
+
+function doWhileLoop(array){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ 
+do {
+  array.pop();
+} while (array.length > 0 && maybeTrue());
+}
